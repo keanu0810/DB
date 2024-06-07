bdc3f3eccc4282c9241ccd6dad3023586896d094
keanu0810 <dsj1201@naver.com>
2024-06-07 17:05:48 +0900

    개념정리

---
+++ SQL_개념정리/16_SYNONYM.sql
@@ -0,0 +1,20 @@
+/*
+SYSNONTM(동의어)
+테이블, 뷰, 시퀀스, 프로시저 와 같은 데이터베이스 객체에 대한 별칭을 생성하는데 사용
+-자바 캡슐화
+
+종류
+
+PUBLIC : 데이터베이스의 모든 사용자가 접근할 수 있는 공용 별칭
+생성
+CREATE PUBLIC STNONYM 별칭명 FOR 객체명;
+삭제
+DROP PUBLIC SYNONYM
+
+
+PRIVATE : 특정 사용자만 접근할 수 있는 개인 별칭
+생성
+CREATE STNONYM 별칭명 FOR 객체명;
+삭제
+DROP SYNONTM 별칭명;
+*/
